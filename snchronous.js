030261fd7069a9373f3919038d03f72c0ca04e9c
MD MAHABUBUR RAHMAN <rahmanmdmahabubur666@gmail.com>
2025-08-05 22:51:39 +0600

    js

---
+++ snchronous.js
@@ -0,0 +1,18 @@
+console.log("maha")
+console.log("time")
+console.log(30)
+console.log("ls")
+
+let fun1 = ()=>{
+    console.log("fun1")
+}
+let fun2 = ()=>{
+    console.log("fun2")
+}
+let fun3 = ()=>{
+    console.log("fun3")
+}
+
+fun1();
+fun3();
+fun2();

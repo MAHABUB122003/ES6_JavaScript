7b62a39f38de3169a536edc8eb7715ce96265139
MD MAHABUBUR RAHMAN <rahmanmdmahabubur666@gmail.com>
2025-08-05 21:48:47 +0600

    second

---
+++ ForEach.js
@@ -0,0 +1,36 @@
+
+// for(var i = 0; i<=10; i++){
+//     console.log(i)
+// }
+
+// const arr = [1,2,3,4,5]
+
+// for(array of arr){
+//     console.log(array)
+// }
+
+
+let mahabub = [1,2,3,4,5,6,7];
+
+mahabub.forEach(fun);
+
+function fun(number){
+    console.log(number)
+}
+
+let hasan = ['a', 'b', 'c', 'd']
+
+hasan.forEach(time)
+
+function time(num){
+    console.log(num)
+}
+
+// arrow function
+
+let emon = ["Mahabub", "hasan", "emon"]
+
+emon.forEach((x)=>console.log(x));
+emon.forEach((x)=>console.log(x))
+emon.forEach((m)=>console.log(m))
+emon.forEach((n)=>console.log(n))
